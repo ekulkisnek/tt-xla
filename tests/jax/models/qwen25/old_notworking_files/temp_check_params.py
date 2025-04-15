112b807b1af4fa91fff59c210f666705a57b76f6
ekulkisnek <lkensik8@gmail.com>
2025-04-15 11:42:29 -0500

    removed old notworking files cause annoying

--- tests/jax/models/qwen25/old_notworking_files/temp_check_params.py
+++
@@ -1,6 +0,0 @@
-from safetensors import safe_open
-
-with safe_open('/Users/lu/Documents/tt-bounty-1/qwen2.5-7b/model-00001-of-00004.safetensors', framework='numpy', device='cpu') as f:
-    keys = sorted(f.keys())[:30]
-    for key in keys:
-        print(key) 
